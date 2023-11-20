5ecf39ac303f39305b1bcffaf15d352c1083d47a
Belish88 <belish888@rambler.ru>
2023-11-20 19:53:49 +0400

    cache

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
@@ -7,7 +8,7 @@ app_name = BlogConfig.name
 
 
 urlpatterns = [
-    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+    path('blog_list/', cache_page(60)(BlogListView.as_view()), name='blog_list'),
     path('<int:pk>/blog/', BlogDetailView.as_view(), name='blog'),
     path('create/', BlogCreateView.as_view(), name='create_blog'),
     path('update/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
